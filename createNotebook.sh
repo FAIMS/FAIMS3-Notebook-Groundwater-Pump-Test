418909576e3c4f5cfd95a598cbb02967ed820254
Brian Ballsun-Stanton <denubis@noreply.users.github.com>
2022-07-22 16:42:59 +1000

    hrid
    Signed-off-by: Brian Ballsun-Stanton <denubis@noreply.users.github.com>

--- createNotebook.sh
+++ createNotebook.sh
@@ -2,11 +2,10 @@
 
 set -euo pipefail
 
-export SERVER_FULLNAME="${1:-dev.db.faims.edu.au}" # dev, alpha, testing, selenium
-cp csirogeochem.json faims3-temp-notebook-migrator/
+export SERVER_FULLNAME="${1:-demo.db.faims.edu.au}" # dev, alpha, testing, selenium
+cp notebook.json faims3-temp-notebook-migrator/
 cd faims3-temp-notebook-migrator
 echo "Load Geochem ${SERVER_FULLNAME}"
 #sed -e "s/##SPID##/${SPID}/g" 20220110-model1.json > fiptemp.json
-bash loadNotebook.sh $SERVER_FULLNAME "CSIRO Geochem $(date)" "csirogeochem.json" "csirogeochem"
-rm -f faims3-temp-notebook-migrator/csirogeochem.json
-rm -f faims3-temp-notebook-migrator/testnotebook.json
+bash loadNotebook.sh $SERVER_FULLNAME "Groundwater Pump Test $(date)" "notebook.json" "groundwater_pump_test"
+rm -f faims3-temp-notebook-migrator/notebook.json
